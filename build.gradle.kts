5b9b9e0b83b57b9eb824dc8b826f3b1330c6a9b0
DeLitX <dimkanan@gmail.com>
2022-10-23 17:19:09 +0300

    Fixed startup

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 group = "com.delitx"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
@@ -32,7 +32,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "16"
+        jvmTarget = "11"
     }
 }
 
